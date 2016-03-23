26f1a1cae2b23dab58a0b9d7623c033e36c07d18
Kit Langton <kitlangton@gmail.com>
2016-03-23 18:44:20 -0400

    fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:4200'
+    origins 'localhost:4200', 'https://damp-tundra-99936.herokuapp.com'
 
     resource '*',
       headers: :any,
